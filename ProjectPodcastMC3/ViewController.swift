3882efab6d7145015a75d61da699c75a38d836e6
Reynaldi <reynaldiwijata2306@gmail.com>
2020-07-27 18:01:23 +0700

    Initial Commit

---
+++ ProjectPodcastMC3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProjectPodcastMC3
+//
+//  Created by Reynaldi Wijaya on 27/07/20.
+//  Copyright © 2020 Reynaldi Wijaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
