cf2e549fb400384d52179a9a6dc3577a483a7530
Reynaldi <reynaldiwijata2306@gmail.com>
2020-08-04 23:54:00 +0700

    style/get-the-audio
    Create The User Interface for search podcast and Upload Podcast

---
+++ ProjectPodcastMC3/Utils/Colors.swift
@@ -0,0 +1,9 @@
+//
+//  Colors.swift
+//  ProjectPodcastMC3
+//
+//  Created by Reynaldi Wijaya on 04/08/20.
+//  Copyright © 2020 Reynaldi Wijaya. All rights reserved.
+//
+
+import Foundation
---
+++ ProjectPodcastMC3/Your Audio/Views/SearchPodcastView.swift
@@ -0,0 +1,21 @@
+//
+//  SearchPodcastView.swift
+//  ProjectPodcastMC3
+//
+//  Created by Reynaldi Wijaya on 04/08/20.
+//  Copyright © 2020 Reynaldi Wijaya. All rights reserved.
+//
+
+import UIKit
+
+class SearchPodcastView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
