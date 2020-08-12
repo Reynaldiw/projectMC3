84b5095cc5c3d3023a33ed475aa097aed1785842
NaniSukmaP <luckyputri98@gmail.com>
2020-08-12 18:48:18 +0700

    Login AppleID

---
+++ ProjectPodcastMC3/LoginAppleID/LoginViewController.swift
@@ -0,0 +1,168 @@
+//
+//  LoginViewController.swift
+//  ProjectPodcastMC3
+//
+//  Created by Nani Sukma Putri Pratama on 12/08/20.
+//  Copyright © 2020 Reynaldi Wijaya. All rights reserved.
+//
+
+import UIKit
+import AuthenticationServices
+
+class LoginViewController: UIViewController {
+    
+  
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor(red: 1/255, green: 0/255, blue: 17/255, alpha: 1.0)
+        setupProviderLoginView()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        performExistingAccountSetupFlows()
+    }
+    
+    /// - Tag: add_appleid_button
+    func setupProviderLoginView() {
+        let authorizationButton = ASAuthorizationAppleIDButton(type: .signIn, style: .white)
+//        //hanya view tata letak
+//        authorizationButton.tintColor = .white
+//        authorizationButton.backgroundColor = .white
+        
+         view.addSubview(authorizationButton)
+        authorizationButton.translatesAutoresizingMaskIntoConstraints = false
+        authorizationButton.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor).isActive = true
+        authorizationButton.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor, multiplier: 0.6).isActive = true
+        authorizationButton.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.05).isActive = true
+        authorizationButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -160).isActive = true
+        //ini batas view tataletak
+        authorizationButton.addTarget(self, action: #selector(handleAuthorizationAppleIDButtonPress), for: .touchUpInside)
+       
+    }
+    
+    // - Tag: perform_appleid_password_request
+    /// Prompts the user if an existing iCloud Keychain credential or Apple ID credential is found.
+    func performExistingAccountSetupFlows() {
+        // Prepare requests for both Apple ID and password providers.
+        let requests = [ASAuthorizationAppleIDProvider().createRequest(),
+                        ASAuthorizationPasswordProvider().createRequest()]
+        
+        // Create an authorization controller with the given requests.
+        let authorizationController = ASAuthorizationController(authorizationRequests: requests)
+        authorizationController.delegate = self
+        authorizationController.presentationContextProvider = self
+        authorizationController.performRequests()
+    }
+    
+    /// - Tag: perform_appleid_request
+    @objc
+    func handleAuthorizationAppleIDButtonPress() {
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+        let request = appleIDProvider.createRequest()
+        request.requestedScopes = [.fullName, .email]
+        
+        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+        authorizationController.delegate = self
+        authorizationController.presentationContextProvider = self
+        authorizationController.performRequests()
+    }
+}
+
+extension LoginViewController: ASAuthorizationControllerDelegate {
+    /// - Tag: did_complete_authorization
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        switch authorization.credential {
+        case let appleIDCredential as ASAuthorizationAppleIDCredential:
+            
+            // Create an account in your system.
+            let userIdentifier = appleIDCredential.user
+            let fullName = appleIDCredential.fullName
+            let email = appleIDCredential.email
+            
+            var identityToken : String?
+            if let token = appleIDCredential.identityToken{
+                identityToken = String(bytes: token, encoding: .utf8)
+            }
+            var authorizationCode  : String?
+            if let code = appleIDCredential.authorizationCode{
+                authorizationCode = String(bytes: code, encoding: .utf8)
+                
+            }
+            
+            print("identity token = \(identityToken)")
+            print(" authorization code = \(authorizationCode)")
+            // For the purpose of this demo app, store the `userIdentifier` in the keychain.
+            self.saveUserInKeychain(userIdentifier)
+            
+            // For the purpose of this demo app, show the Apple ID credential information in the `ResultViewController`.
+//            self.showResultViewController(userIdentifier: userIdentifier, fullName: fullName, email: email)
+            let podcastView = PodcastContentController()
+            podcastView.modalPresentationStyle = .fullScreen
+            present(podcastView, animated: true, completion: nil)
+        
+        case let passwordCredential as ASPasswordCredential:
+        
+            // Sign in using an existing iCloud Keychain credential.
+            let username = passwordCredential.user
+            let password = passwordCredential.password
+            
+            // For the purpose of this demo app, show the password credential as an alert.
+            DispatchQueue.main.async {
+                self.showPasswordCredentialAlert(username: username, password: password)
+            }
+            
+        default:
+            break
+        }
+    }
+    
+    private func saveUserInKeychain(_ userIdentifier: String) {
+        do {
+            try KeychainItem(service: "com.example.apple-samplecode.juice", account: "userIdentifier").saveItem(userIdentifier)
+        } catch {
+            print("Unable to save userIdentifier to keychain.")
+        }
+    }
+    
+//    private func showResultViewController(userIdentifier: String, fullName: PersonNameComponents?, email: String?) {
+//        guard let viewController = self.presentingViewController as? ResultViewController
+//            else { return }
+//
+//        DispatchQueue.main.async {
+//            viewController.userIdentifierLabel.text = userIdentifier
+//            if let givenName = fullName?.givenName {
+//                viewController.givenNameLabel.text = givenName
+//            }
+//            if let familyName = fullName?.familyName {
+//                viewController.familyNameLabel.text = familyName
+//            }
+//            if let email = email {
+//                viewController.emailLabel.text = email
+//            }
+//            self.dismiss(animated: true, completion: nil)
+//        }
+//    }
+    
+    private func showPasswordCredentialAlert(username: String, password: String) {
+        let message = "The app has received your selected credential from the keychain. \n\n Username: \(username)\n Password: \(password)"
+        let alertController = UIAlertController(title: "Keychain Credential Received",
+                                                message: message,
+                                                preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler: nil))
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    /// - Tag: did_complete_error
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+        // Handle error.
+    }
+}
+
+extension LoginViewController: ASAuthorizationControllerPresentationContextProviding {
+    /// - Tag: provide_presentation_anchor
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        return self.view.window!
+    }
+}
